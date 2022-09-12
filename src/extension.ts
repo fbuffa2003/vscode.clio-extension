1a7fd05ed01686f6c36df5b0af411899794b9c05
Kirill Krylov <k.krylov@creatio.com>
2022-09-12 20:22:37 +0300

    added flushdb and restart commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,24 @@
 import * as vscode from 'vscode';
 import { fileURLToPath } from 'url';
+import {EnvironmentService} from './service/environmentService';
+
 
 let terminal: vscode.Terminal | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
 	
+	const clioPath = 'C:\\Users\\k.krylov.TSCRM\\.dotnet\\tools\\clio.exe';
+
+	const envService = new EnvironmentService();
+	vscode.window.registerTreeDataProvider('vscode-clio-extension.creatioExplorer', envService);
+
 	let disposable = vscode.commands.registerCommand('ClioSQL.console', () => {
 		let commandsDocument = vscode.window.activeTextEditor?.document;
 		let text : string = commandsDocument?.getText() as string;
 		if(!text){
 			return;
 		}
-        let filePath = 'E:\\Projects\\clio\\clio\\bin\\Release\\netcoreapp3.1\\clio.exe';
+		let filePath = 'E:\\Projects\\clio\\clio\\bin\\Release\\netcoreapp3.1\\clio.exe';
 		let isWin = process.platform === 'win32';
 		terminal = terminal || vscode.window.createTerminal('clio sql console', isWin ? 'C:\\Windows\\System32\\cmd.exe' : undefined);
 		terminal.show();
@@ -24,5 +31,40 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	
+
+	let restartCommand = vscode.commands.registerCommand('ClioSQL.restart', (node: vscode.TreeItem) => {
+		if(!node.label){
+			return;
+		}
+		let isWin = process.platform === 'win32';
+		terminal = terminal || vscode.window.createTerminal('clio sql console', isWin ? 'C:\\Windows\\System32\\cmd.exe' : undefined);
+		terminal.show();
+		terminal.sendText(`${isWin ? '' : 'wine '}"${clioPath}"  restart -e "${node.label}"`);
+		vscode.window.onDidCloseTerminal(closedTerminal => {
+			if (closedTerminal === terminal) {
+				terminal = undefined;
+			}
+		});
+	});
+	context.subscriptions.push(restartCommand);
+
+
+	let flushdbCommand = vscode.commands.registerCommand('ClioSQL.flushDb', (node: vscode.TreeItem) => {
+		if(!node.label){
+			return;
+		}
+		let isWin = process.platform === 'win32';
+		terminal = terminal || vscode.window.createTerminal('clio sql console', isWin ? 'C:\\Windows\\System32\\cmd.exe' : undefined);
+		terminal.show();
+		terminal.sendText(`${isWin ? '' : 'wine '}"${clioPath}"  flushdb -e "${node.label}"`);
+		vscode.window.onDidCloseTerminal(closedTerminal => {
+			if (closedTerminal === terminal) {
+				terminal = undefined;
+			}
+		});
+	});
+	context.subscriptions.push(flushdbCommand);
+
 }
 export function deactivate() {}
---
+++ src/service/environmentService.ts
@@ -0,0 +1,79 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+export class EnvironmentService implements vscode.TreeDataProvider<CreatioInstance>{
+	
+	private _onDidChangeTreeData: vscode.EventEmitter<CreatioInstance | undefined | void> = new vscode.EventEmitter<CreatioInstance | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<CreatioInstance | undefined | void> = this._onDidChangeTreeData.event;
+	
+	refresh(): void {
+		this._onDidChangeTreeData?.fire();
+	}
+
+	getTreeItem(element: CreatioInstance): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return element;
+	}
+	
+	getChildren(element?: CreatioInstance | undefined): vscode.ProviderResult<CreatioInstance[]> {
+		let settingsFile = this.getClioEnvironments();
+		let json = JSON.parse(settingsFile);
+		const environments = json['Environments'];
+		if(element === undefined){
+			let instances : Array<CreatioInstance> = [];
+			let keys : string[] = [];
+			Object.keys(environments).forEach(key =>{
+				keys.push(key);
+			});
+			keys.forEach(key=>{
+				type ObjectKey = keyof typeof environments;
+				const keyName = key as ObjectKey;
+				const environment = environments[keyName];
+				let instance:CreatioInstance = new CreatioInstance(key, environment['Uri'], vscode.TreeItemCollapsibleState.Collapsed);
+				instances.push(instance);
+			});
+			return Promise.resolve(instances);
+		}
+		return Promise.resolve([]);
+	}
+
+
+	getClioEnvironments() : string {
+
+		let file = fs.readFileSync(
+			path.join("C:\\Users\\k.krylov.TSCRM\\AppData\\Local\\creatio\\clio\\appsettings.json"),
+			{
+				encoding: "utf-8"
+			}
+		);
+
+		return file;
+	}
+
+}
+
+
+
+export class CreatioInstance extends vscode.TreeItem {
+
+	constructor(
+		public readonly label: string,
+		public readonly description: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState
+	) {
+		super(label, collapsibleState);
+		this.tooltip = label;
+		this.description = description;
+	}
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	};
+	contextValue = 'CreatioInstace';
+}
+
+class EnvironmentModel {
+	
+	public name?: string;
+	public uri?: string;
+}
